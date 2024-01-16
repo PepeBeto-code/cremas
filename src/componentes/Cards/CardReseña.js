eb6cfaf54eb174c4bb74e7e57e3aa21a9bad1037
Jose Alberto Miñon Velazquez <jose-alberto@ciencias.unam.mx>
2024-01-15 18:48:56 -0600

    hidratantes

--- src/componentes/Cards/CardReseña.js
+++ src/componentes/Cards/CardReseña.js
@@ -4,19 +4,21 @@ import Image from 'next/image';
 import styles from "./Card.module.css"
 function CardReseña(props) {
     return (
+        <a className={styles.link} href='/reafirmantes'>
         <article className='flex justify-content-end align-items-center flex-col shadow-2xl'>
             <Image
               src={Logo}
               className='w-full'
               alt="Imagen Plan Funerario"
             />
-            <h3>Titulo Reseña</h3>
-            <p className={`${styles.texto} md:block hidden`}>gsnshj jhsjhsjhdsj jhsjhsjhdsj
+            <h3 className={`${styles.h3}`}>Titulo Reseña</h3>
+            <p className={`md:block hidden pt-[0px]`}>gsnshj jhsjhsjhdsj jhsjhsjhdsj
                 hsjhjhs hjdshjsh hsjhdjsdhjss hs hsjshjhdssd
                 jshjshds hd jhsjh hsj hsjhdjhs hsdjhsd hs
             </p>
-            <a className={styles.link} href='/reafirmantes'>Leer Mas</a>
+            
         </article>
+        </a>
     );
 }
 
